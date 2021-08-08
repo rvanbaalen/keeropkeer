0c04f5da7d66afc1a5370cc793aa02a1483e8c9f
Robin <accounts@ganttify.com>
2021-08-08 13:53:55 -0400

    fix
    Signed-off-by: Robin <accounts@ganttify.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 let scoreBlocks = document.getElementsByClassName('score-block');
 Array.prototype.forEach.call(scoreBlocks, (scoreBlock) => {
     scoreBlock.addEventListener('click', (event) => {
-        event.targetElement.classList.toggle('selected');
+        scoreBlock.classList.toggle('selected');
         console.log(event.targetElement, 'toggled');
     }, false);
 });
