93e65fc64ec12f6770b08e0f47be27032d2c7be0
Robin <accounts@ganttify.com>
2021-08-08 14:03:08 -0400

    here you go
    Signed-off-by: Robin <accounts@ganttify.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,21 @@ let scoreBlocks = document.getElementsByClassName('score-block');
 Array.prototype.forEach.call(scoreBlocks, (scoreBlock) => {
     scoreBlock.addEventListener('click', (event) => {
         scoreBlock.classList.toggle('selected');
-        console.log(event.targetElement, 'toggled');
+    }, false);
+});
+
+let columnScores = document.getElementsByClassName('column-score');
+Array.prototype.forEach.call(columnScores, (columnScore) => {
+    columnScore.addEventListener('click', (event) => {
+        if (!columnScore.classList.contains('active') && !columnScore.classList.contains('taken')) {
+            columnScore.classList.add('active');
+            columnScore.classList.remove('taken');
+        } else if (columnScore.classList.contains('active') && !columnScore.classList.contains('taken')) {
+            columnScore.classList.add('taken');
+            columnScore.classList.remove('active');
+        } else {
+            columnScore.classList.remove('taken');
+            columnScore.classList.remove('active');
+        }
     }, false);
 });
