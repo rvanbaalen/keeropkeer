16e09367dd5c64fe16d4bdca26266c865fa944b5
Robin <accounts@ganttify.com>
2021-08-08 13:53:04 -0400

    js
    Signed-off-by: Robin <accounts@ganttify.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 let scoreBlocks = document.getElementsByClassName('score-block');
-scoreBlocks.forEach((scoreBlock) => {
+Array.prototype.forEach.call(scoreBlocks, (scoreBlock) => {
     scoreBlock.addEventListener('click', (event) => {
         event.targetElement.classList.toggle('selected');
         console.log(event.targetElement, 'toggled');
