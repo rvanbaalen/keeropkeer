a0f18ac0d5f3fb66e52bb639ac2ba8f1d042cce6
Robin <accounts@ganttify.com>
2021-08-10 15:05:28 -0400

    persistent state and levels!
    Signed-off-by: Robin <accounts@ganttify.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,199 @@
 const JOKER_VALUE = 1;
 const STAR_VALUE = 2;
+const TOTAL_JOKERS = 8;
 
-window.onbeforeunload = unload;
+function createElement(el, options, appendTo, listen = []){
+    let element = document.createElement(el);
+    Object.keys(options).forEach(function (k){
+        element[k] = options[k];
+    });
+    if (listen.length > 0){
+        listen.forEach(function(l){
+            element.addEventListener(l.event, l.f);
+        });
+    }
+    if (appendTo) {
+        appendTo.append(element);
+    }
 
-function unload() {
- return confirm('Weet je zeker dat je weg wil gaan?');
-};
-
-let scoreBlocks = document.querySelectorAll('.score-block:not(.final-score)');
-Array.prototype.forEach.call(scoreBlocks, (scoreBlock) => {
-    scoreBlock.addEventListener('click', () => {
-        toggleSelected(scoreBlock);
-        updateScores();
-        setStarTotal();
-    }, false);
-});
-
-function toggleSelected(block) {
-    block.classList.toggle('selected');
+    return element;
 }
 
-let finalScores = document.querySelectorAll('.final-score');
-Array.prototype.forEach.call(finalScores, (finalScore) => {
-    finalScore.addEventListener('click', () => {
-        finalScore.classList.toggle('final-selected');
-        setBonusTotal();
-    }, false);
-});
+function parseColumn(column) {
+    let columnTemplate = createElement('div', {className: 'column' + (column.column === 'H' ? ' highlight' : '')});
+    // create header
+    createElement('span', {className: 'letter rounded-block', innerText: column.column}, columnTemplate);
+
+    // create grid blocks
+    column.grid.forEach((block, index) => {
+        const row = createElement('span', {
+            className: 'score-block ' + block.color,
+            data: {
+                column: column.column,
+                row: index
+            }
+        });
+        if (block.star) {
+            createElement('span', {className: 'star', innerText: '*'}, row);
+        }
+        if (block.selected) {
+            row.classList.add('selected');
+        }
+
+        columnTemplate.append(row);
+    });
+    // create score columns
+    let score = createElement('div', {className: 'column-score'});
+    column.score.forEach((scoreObject, index) => {
+        let state = (scoreObject.state && scoreObject.state !== 'default') ? ' ' + scoreObject.state : '';
+        createElement('span', {
+            className: 'rounded-block column-score' + state,
+            innerText: scoreObject.value,
+            data: {
+                column: column.column,
+                row: index
+            }
+        }, score);
+    });
+    columnTemplate.append(score);
+
+    return columnTemplate;
+}
+
+let selectedLevel = level1;
+function getSelectedLevel() {
+    if (!selectedLevel) {
+        let level = prompt('Kies een level (1-3)', '1');
+        switch (level) {
+            default:
+                selectedLevel = level1;
+        }
+    }
+
+    return selectedLevel;
+}
+function createState() {
+    let state = {
+        grid: getSelectedLevel(),
+        jokers: []
+    }, i;
+    for (i = 0; i < TOTAL_JOKERS; i++) {
+        state.jokers.push({selected: false});
+    }
+
+    return state;
+}
+function resetState() {
+    localStorage.removeItem('kok_state');
+    window.location.reload();
+
+    return true;
+}
+function getState() {
+    return JSON.parse(localStorage.getItem('kok_state')) || createState();
+}
+function setState(state) {
+    localStorage.setItem('kok_state', JSON.stringify(state));
+}
+function updateState(column, index, key, value, type = 'grid') {
+    let currentState = getState();
+    let found = false;
+    currentState.grid.forEach((stateColumn, stateIndex) => {
+        if (stateColumn.column === column) {
+            currentState.grid[stateIndex][type][index][key] = value;
+            found = true;
+        }
+    });
+
+    if (found) {
+        setState(currentState);
+    }
+
+    render(currentState);
+}
+function updateBlockState(col, row, key, value) {
+    let currentState = getState();
+    let found = false;
+    currentState.grid.forEach((stateColumn, stateIndex) => {
+        if (stateColumn.column === col) {
+            currentState.grid[stateIndex].grid[row][key] = value;
+            found = true;
+        }
+    });
+
+    if (found) {
+        setState(currentState);
+    }
+
+    render(currentState);
+}
+function updateJokerState(row, selected) {
+    let currentState = getState();
+    let found = false;
+    currentState.jokers.forEach((joker, index) => {
+        if (index === row) {
+            joker.selected = selected;
+            found = true;
+        }
+    });
+
+    if (found) {
+        setState(currentState);
+    }
+
+    render(currentState);
+}
+
+function registerEventListeners() {
+    // Colored blocks
+    let scoreBlocks = document.querySelectorAll('.score-block:not(.final-score)');
+    Array.prototype.forEach.call(scoreBlocks, (scoreBlock) => {
+        scoreBlock.addEventListener('click', () => {
+            let blockSelected = !scoreBlock.classList.contains('selected');
+            updateBlockState(scoreBlock.data.column, scoreBlock.data.row, 'selected', blockSelected);
+            updateScores();
+            setStarTotal();
+        }, false);
+    });
+
+    // Final score toggles
+    let finalScores = document.querySelectorAll('.final-score');
+    Array.prototype.forEach.call(finalScores, (finalScore) => {
+        finalScore.addEventListener('click', () => {
+            finalScore.classList.toggle('final-selected');
+            setBonusTotal();
+        }, false);
+    });
+
+    // Column scores
+    let columnScores = document.querySelectorAll('span.column-score');
+    Array.prototype.forEach.call(columnScores, (columnScore) => {
+        columnScore.addEventListener('click', () => {
+            let col = columnScore.data.column, row = columnScore.data.row, state;
+
+            if (!columnScore.classList.contains('active') && !columnScore.classList.contains('taken')) {
+                state = 'active';
+            } else if (columnScore.classList.contains('active') && !columnScore.classList.contains('taken')) {
+                state = 'taken';
+            } else {
+                state = 'default';
+            }
+
+            updateState(col, row, 'state', state, 'score');
+            setColumnTotal();
+        }, false);
+    });
+
+    // Jokers
+    let jokers = document.getElementsByClassName('joker');
+    Array.prototype.forEach.call(jokers, (joker, index) => {
+        joker.addEventListener('click', () => {
+            let state = !joker.classList.contains('used');
+            updateJokerState(index, state);
+            setJokerTotal();
+        }, false);
+    });
+}
 
 let claimed = [];
 function updateScores() {
@@ -72,32 +239,6 @@ function updateScores() {
     });
 }
 
-let columnScores = document.getElementsByClassName('column-score');
-Array.prototype.forEach.call(columnScores, (columnScore) => {
-    columnScore.addEventListener('click', () => {
-        if (!columnScore.classList.contains('active') && !columnScore.classList.contains('taken')) {
-            columnScore.classList.add('active');
-            columnScore.classList.remove('taken');
-        } else if (columnScore.classList.contains('active') && !columnScore.classList.contains('taken')) {
-            columnScore.classList.add('taken');
-            columnScore.classList.remove('active');
-        } else {
-            columnScore.classList.remove('taken');
-            columnScore.classList.remove('active');
-        }
-
-        setColumnTotal();
-    }, false);
-});
-
-let jokers = document.getElementsByClassName('joker');
-Array.prototype.forEach.call(jokers, (joker) => {
-    joker.addEventListener('click', () => {
-        joker.classList.toggle('used');
-        setJokerTotal();
-    }, false);
-});
-
 function getTotalScore() {
     return getBonusTotal() + getColumnTotal() + getJokerTotal() - getStarTotal();
 }
@@ -131,13 +272,11 @@ function getJokerTotal() {
 function setJokerTotal() {
     $('jokerTotal').innerText = getJokerTotal();
 }
-
 function getColumnTotal() {
     let activeColumns = document.querySelectorAll('span.column-score.active');
     let totalValue = 0;
     Array.prototype.forEach.call(activeColumns, (activeColumn) => {
         let value = parseInt(activeColumn.innerText);
-        console.log(value);
         totalValue += value;
     });
 
@@ -160,11 +299,39 @@ function setStarTotal() {
 function $(id) {
     return document.getElementById(id);
 }
-function init() {
+function render(state) {
+    if (typeof state === 'undefined') {
+        state = getState();
+    }
+
+    $('blockGrid').innerHTML = '';
+    state.grid.forEach(column => {
+        $('blockGrid').append(parseColumn(column));
+    });
+
+    $('jokerContainer').innerHTML = '';
+    state.jokers.forEach(joker => {
+        let renderedJoker = createElement('span', {className: 'joker', innerText: '!'});
+        if (joker.selected) {
+            renderedJoker.classList.add('used');
+        }
+        $('jokerContainer').append(renderedJoker);
+    });
+
+    registerEventListeners();
     setBonusTotal()
     setJokerTotal();
     setColumnTotal();
     setStarTotal();
+}
+function init() {
+    render();
+
+    $('newGame').addEventListener('click', function () {
+        if (confirm('Weet je zeker dat je een nieuw spel wil starten?')) {
+            resetState();
+        }
+    }, false);
 }
 
 $('totals').addEventListener('click', () => {
---
+++ assets/js/levels.js
@@ -0,0 +1,512 @@
+const level1 = [
+    {
+        column: 'A',
+        score: [
+            {
+                value: 5
+            },{
+                value: 3
+            }
+        ],
+        grid: [
+            {
+                color: 'green'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'blue',
+                star: true
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        column: 'B',
+        score: [
+            {
+                value: 3
+            },{
+                value: 2
+            }
+        ],
+        grid: [
+            {
+                color: 'green'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'blue',
+                star: true
+            },
+            {
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        column: 'C',
+        score: [
+            {
+                value: 3
+            },{
+                value: 2
+            }
+        ],
+        grid: [
+            {
+                color: 'green'
+            },
+            {
+                color: 'yellow',
+                star: true
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        column: 'D',
+        score: [
+            {
+                value: 3
+            },{
+                value: 2
+            }
+        ],
+        grid: [
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'red',
+                star: true
+            },
+            {
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        column: 'E',
+        score: [
+            {
+                value: 2
+            },{
+                value: 1
+            }
+        ],
+        grid: [
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'yellow',
+                star: true
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        column: 'F',
+        score: [
+            {
+                value: 2
+            },{
+                value: 1
+            }
+        ],
+        grid: [
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'orange',
+                star: true
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        column: 'G',
+        score: [
+            {
+                value: 2
+            },{
+                value: 1
+            }
+        ],
+        grid: [
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'green',
+                star: true
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'red'
+            }
+        ]
+    },
+    {
+        column: 'H',
+        score: [
+            {
+                value: 1
+            },{
+                value: 0
+            }
+        ],
+        grid: [
+            {
+                color: 'green',
+                star: true
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        column: 'I',
+        score: [
+            {
+                value: 2
+            },{
+                value: 1
+            }
+        ],
+        grid: [
+            {
+                color: 'blue'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'yellow',
+                star: true
+            },
+            {
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        column: 'J',
+        score: [
+            {
+                value: 2
+            },{
+                value: 1
+            }
+        ],
+        grid: [
+            {
+                color: 'blue'
+            },
+            {
+                color: 'blue',
+                star: true
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        column: 'K',
+        score: [
+            {
+                value: 2
+            },{
+                value: 1
+            }
+        ],
+        grid: [
+            {
+                color: 'blue'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'red',
+                star: true
+            },
+            {
+                color: 'green'
+            }
+        ]
+    },
+    {
+        column: 'L',
+        score: [
+            {
+                value: 2
+            },{
+                value: 1
+            }
+        ],
+        grid: [
+            {
+                color: 'orange',
+                star: true
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'green'
+            }
+        ]
+    },
+    {
+        column: 'M',
+        score: [
+            {
+                value: 3
+            },{
+                value: 2
+            }
+        ],
+        grid: [
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'orange'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'green',
+                star: true
+            }
+        ]
+    },
+    {
+        column: 'N',
+        score: [
+            {
+                value: 3
+            },{
+                value: 2
+            }
+        ],
+        grid: [
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'red',
+                star: true
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'orange'
+            }
+        ]
+    },
+    {
+        column: 'O',
+        score: [
+            {
+                value: 5
+            },{
+                value: 3
+            }
+        ],
+        grid: [
+            {
+                color: 'yellow'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'green'
+            },
+            {
+                color: 'blue'
+            },
+            {
+                color: 'red'
+            },
+            {
+                color: 'orange',
+                star: true
+            },
+            {
+                color: 'orange'
+            }
+        ]
+    }
+];
