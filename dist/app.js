710e4f2a11aa2cc442a02b82afd73ec03119d597
Robin <accounts@ganttify.com>
2021-09-06 14:33:49 -0400

    build release
    Signed-off-by: Robin <accounts@ganttify.com>

--- dist/app.js
+++ dist/app.js
@@ -334,7 +334,7 @@ function createNewModal(options) {
     return modal;
 }
 
-const SOCKET_SERVER = 'http://192.168.1.111:3000/';
+const SOCKET_SERVER = 'https://dry-peak-80209.herokuapp.com/' ;
 
 const io = window.io;
 const socket = io(SOCKET_SERVER, { autoConnect: false });
